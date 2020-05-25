1883ff568996841ed81b136fd53cb093574c4d26
Devin Whitten <dwhitten@nd.edu>
2020-05-25 18:15:29 -0500

    update main

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-### Name: Devin D. Whitten
-### Email: dwhitten@nd.edu
+################################################################################
+### Author: Devin Whitten
+### Email: devin.d.whitten@gmail.com
 ### Institute: University of Notre Dame
+################################################################################
 
-### CCSLab_main.py
+### CASPER
 
 ####
-#spectra_path  = 'inputs/spectra/gmos-ft2017a-reduced/'
 spectra_path  = 'inputs/spectra/'
 param_path    = 'params/param_file_trun.dat'
-#param_path    = 'params/gmos-ft2017a-casper-input.csv'
+
 
 io_param_path = 'params/io_param.py'
 ####
@@ -29,24 +30,24 @@ start_time = time.time()
 print("... initializing spectra batch")
 spec_batch = Batch(spectra_path, param_path, io_param_path)
 
-### load spectra
+################################################################################
+### load spectra + params
 spec_batch.load_params()
 spec_batch.load_spectra(is_fits=True)
 spec_batch.set_params()
-
 
 io_functions.span_window()
 
 #spec_batch.radial_correct()
 spec_batch.build_frames()
 
-
-### normalize with GISIC
 io_functions.span_window()
 
-
+################################################################################
+#### Continuum normalization with GISIC
 spec_batch.normalize()
 
+################################################################################
 #### Preliminaries
 spec_batch.set_KP_bounds()
 spec_batch.set_carbon_mode()
@@ -54,21 +55,18 @@ spec_batch.set_carbon_mode()
 spec_batch.estimate_sn()
 spec_batch.ebv_correction()
 
-
+################################################################################
+#### Main procedures
 spec_batch.calibrate_temperatures()
 
-
-##### big functions
 spec_batch.archetype_classification()
 
 spec_batch.mcmc_determination(pool=20)
 
+################################################################################
 ##### generate output files
-
 spec_batch.generate_synthetic()
-
 spec_batch.generate_plots()
-
 spec_batch.generate_output_files()
 
 print("The total time for this CASPER run is {:.2f}s".format(time.time()-start_time))
