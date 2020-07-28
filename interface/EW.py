40e1aefbf7dee38adf7c3299c8dca1b4af305b11
Jinmi Yoon <jyoon4@nd.edu>
2020-07-28 09:09:43 -0400

    Update EW.py

--- interface/EW.py
+++ interface/EW.py
@@ -29,16 +29,18 @@ def GBAND_QUAD(wave, flux, bounds = [4222, 4322]):
     func = interp1d(wave, 1. - flux)
 
 
-    #################################################################################################
+    ############################################################################
     ## Revised by Jinmi Yoon, July 17 2020
-    ## This revision needed to accomodate the problem of improper normalization of the CH-band area.
-    ## If the CH-band is too low from the continuum level, the EW estimation is not reliable
-    ## and in turn influences the set_CH_procedure whether to use the CH mode or CH+C2 mode.
-    ## The below calculation is needed in set_CH_procedure()
+    ## This revision needed to accomodate the problem of improper normalization
+    ## of the CH-band area.
+    ## If the CH-band is too low from the continuum level, the EW estimation is
+    ## not reliable and in turn influences the set_CH_procedure whether to use
+    ## the CH mode or CH+C2 mode.
+    ## The below calculation is needed in set_CH_procedure().
     ##
-    ## I eventually decided not to use reduced_CH_EW because the synthetic spectra at the CH band
-    ## is indeed lower than 1.0 level. However, I just leave the below scripts as it is for 
-    ## the future debugging.
+    ## I eventually decided not to use reduced_CH_EW because the synthetic
+    ## spectra at the CH band is indeed lower than 1.0 level. However, I just
+    ## leave the below scripts as it is for the future debugging.
     
     func_bounds = interp1d(wave, flux)
     wave_bounds = np.arange(bounds[0], bounds[1], 0.01)
@@ -48,9 +50,9 @@ def GBAND_QUAD(wave, flux, bounds = [4222, 4322]):
     print("flux_bounds_max = ", flux_bounds_max)
 
     ## If flux_boounds_max is not close to 1. (normalized level),
-    ## I will calculate area subtended from 1.0 to flux_bounds_max for later subtraction from CH_EW.
-    ## I need to use SNR at CH band for noise = 1./SNR but it appears very small (~0.02) so at the moment 
-    ##I ignore this.
+    ## I will calculate area subtended from 1.0 to flux_bounds_max for later
+    ## subtraction from CH_EW. I need to use SNR at CH band for noise = 1./SNR
+    ## but it appears very small (~0.02) so at the moment I ignore this.
     
     if flux_bounds_max < 1. :
         EW_subtract = (1.-flux_bounds_max)*(bounds[1]-bounds[0])
@@ -58,10 +60,11 @@ def GBAND_QUAD(wave, flux, bounds = [4222, 4322]):
         EW_subtract = 0.
     print("EW_subtract=  ", EW_subtract)
 
-    #############################################################################################
+    ############################################################################
 
     ## J. Yoon, 07/17/2020
-    ## I added EW_subtract in return so that I can use this subtraction for set_CH_procedure() 
+    ## I added EW_subtract in return so that I can use this subtraction
+    ## for set_CH_procedure().
     
     return integrate.quad(func, bounds[0], bounds[1], limit=1000, 
                           points=list(wave[(wave > bounds[0]) & (wave <  bounds[1])]))[0], EW_subtract
@@ -157,28 +160,29 @@ def set_CH_procedure(spectrum):
     ##### This is intended to check whether C2 Swan band is necessary
     CH_EW, EW_subtract = GBAND_QUAD(spectrum.frame['wave'], spectrum.frame['norm'])
     spectrum.set_GBAND(CH_EW)
-    #print("CH_EW, EW_subtract at EW.py = ", CH_EW, EW_subtract)  # I can print this when I debug later for EW values.
+    #print("CH_EW, EW_subtract at EW.py = ", CH_EW, EW_subtract)
     
-    ########################################################################################################
+    ############################################################################
     # Revised by Jinmi Yoon, July 17 2020
     # The default CH_EW =40 was used for the Yoon+2020 paper,
     # but I realized that EW changes depending on the level of continuum.
-    # So it has to be changed a bit to prevent a false EW value to switch the mode.
-    # I meant to modify GBAND_QUAD calculation slightly to accomodate the problem with this issue.
+    # So it has to be changed a bit to prevent an unnecessarily large EW value
+    # to switch the mode. I meant to modify GBAND_QUAD calculation slightly to
+    # tackle the problem with this issue.
     # However, the problem is that this function appears to be used other places.
-    # So I decided to change critieria here by changing CH_EW value based on the normalization level.
-    # First, I find a highest flux point, flux_max.
-    # If flux_max does not reach 1.0, I subtract area from 1.0 to flux_max level from CH_EW.
-    # To do so I define flux_bounds_max in GBAND_QUAD and calculate this area and feed this number 
-    # in this procedure.
+    # So I decided to change critieria here by changing CH_EW value based on
+    # the normalization level. First, I find a highest flux point, flux_max.
+    # If flux_max does not reach 1.0, I subtract area from 1.0 to flux_max level
+    # from CH_EW. To do so I define flux_bounds_max in GBAND_QUAD and calculate
+    # this area and feed this number in this procedure.
     #
     # reduced_CH_EW = CH_EW - EW_subtract
     # if reduced_CH_EW > 45.:
-    # I decided to keep Devin's procedure because the synthetic spectra at this band 
-    # indeed lower than 1.0 level.
-    ##########################################################################################################
-    # Devin originally used CH_EW >40 for switching however, it depends on the normalization 
-    # though it is likely to be a minor difference. 
+    # I decided to keep Devin's procedure because the synthetic spectra at
+    # this band indeed lower than 1.0 level.
+    ############################################################################
+    # Devin originally used CH_EW >40 for switching however, it depends on
+    # the normalization though it is likely to be a minor difference.
     
     print("CH_EW= %5.2f" %CH_EW)
     if CH_EW > 40.: 
@@ -190,10 +194,6 @@ def set_CH_procedure(spectrum):
         spectrum.set_carbon_mode('CH')
 
     return
-
-
-
-
 
 
 def CAII_KP(wave, flux):
