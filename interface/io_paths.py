bf40afb657a37f46541cd94da4870b8f1f296dbe
jinmiyoon <jinmi.yoon@gmail.com>
2021-06-18 13:00:25 -0400

    added another run results

--- interface/io_paths.py
+++ interface/io_paths.py
@@ -2,13 +2,13 @@
 "plot" : True,
 "normalize" : True,
 # set the directory path where spectra exist for the run
-"spectra_dir_path" : "inputs/spectra/bf-full-survey-data/",
-# set the path for the input setting parameter file
-"param_path"  : "inputs/params/test-carbon-mode-input.csv",
+#"spectra_dir_path" : "inputs/spectra/bf-full-survey-data/",
+"spectra_dir_path" : "inputs/spectra/test_spectra/",
+"param_path"  : "inputs/params/param_file_test.dat",
 # set the output directory path and
 # "output_name" = "output_dir_path"+"output_file_name" in batch.py
 # output_name will be prepended to .pdf and .csv for the spectra fit output files,
 # MCMC corner plots and output stellar parameters files.
 "output_dir_path"   : "outputs/test/",
-"output_file_name"  : "carbon_mode_test"
+"output_file_name"  : "test"
 }
