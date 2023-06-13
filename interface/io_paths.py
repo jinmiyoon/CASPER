5ce1a94691182df5b7dca2ec5c585ee28631c1a2
jinmiyoon <jinmi.yoon@gmail.com>
2023-06-13 08:44:59 -0400

    ran a few cases: hard Teff, carbon mode, etc.

--- interface/io_paths.py
+++ interface/io_paths.py
@@ -7,7 +7,7 @@
 
 #"param_path"  : "/Users/jyoon/Dropbox/research/casper-analysis/input_files/bf-survey-casper-input.csv",
 #"param_path"  : "inputs/params/bf-final-casper-kdemove-input.csv",
-"param_path"  : "inputs/params/mcmc-move-test.csv",
+"param_path"  : "inputs/params/he0017+0055-input.csv",
 #"param_path"  : "inputs/params/bf-validation_casper-input.csv",
 
 # set the output directory path and
@@ -21,5 +21,5 @@
 #"output_file_name"  : "bf-validation_band-check-off_demove0.8-desnooker0.2_walker-100_stepsize-c2-em2-v2"
 #"output_file_name"  : "validation-yoon2020_band-check-off_de-move_walker-100_stepsize-c2-em2-v2"
 #"output_file_name"  : "he0017+0055_cahk-off_band-check-on_demove0.8-desnooker0.2_walker-100_stepsize-c2-em2"
-"output_file_name"  : "mcmc-move-test_nwalker-64_cahk-True_bandcheck-False_GISIC_updated_bands_synthetic"
+"output_file_name"  : "he0017+0055_nwalker-64_cahk-True_bandcheck-False_GISIC_updated_bands_synthetic"
 }
