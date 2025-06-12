e65798b5965ed7a5e6b00fb760293e09e7a373aa
taylorw0525 <taylorw0525@gmail.com>
2025-06-12 16:15:29 -0400

    relocated inputs folder into casper folder. changed the path in the inputs file to start with casper

--- casper/interface/io_paths.py
+++ casper/interface/io_paths.py
@@ -2,10 +2,10 @@
     "plot": True,
     "normalize": True,
     # set the directory path where spectra exist for the run
-    "spectra_dir_path": "inputs/spectra/test_spectra/",
+    "spectra_dir_path": "casper/inputs/spectra/test_spectra/",
     # set input parameter path
-    "param_path": "inputs/params/param_file_test.dat",
+    "param_path": "casper/inputs/params/param_file_test.dat",
     "output_dir_path": "outputs/",
-    "output_file_name": "test",
+    "output_file_name": "sample",
     #
 }
