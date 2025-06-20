1c952a86df7633c930742c1c59e91364a27db1be
taylorw0525 <taylorw0525@gmail.com>
2025-06-20 16:08:14 -0400

    added docstrings for ac.py functions.

--- casper/interface/ac.py
+++ casper/interface/ac.py
@@ -1,6 +1,42 @@
 def ac(cfe: float, feh: float) -> float:
+    """
+    Compute the absolute carbon abundance (A(C)) from [C/Fe] and [Fe/H].
+
+    This function adds the carbon-to-iron ratio ([C/Fe]), the iron-to-hydrogen ratio ([Fe/H]),
+    and a constant (solar carbon abundance, 8.43) to estimate the absolute carbon abundance A(C).
+
+    Parameters
+    ----------
+    cfe : float
+        Logarithmic carbon-to-iron abundance ratio [C/Fe].
+    feh : float
+        Logarithmic iron-to-hydrogen abundance ratio [Fe/H].
+
+    Returns
+    -------
+    float
+        The absolute carbon abundance A(C).
+    """
     return cfe + feh + 8.43
 
 
 def cfe(ac: float, feh: float) -> float:
+    """
+    Calculate the carbon-to-iron abundance ratio ([C/Fe]) from absolute carbon abundance.
+
+    This function subtracts the solar carbon abundance (8.43) and the iron-to-hydrogen
+    ratio ([Fe/H]) from the absolute carbon abundance (A(C)) to estimate [C/Fe].
+
+    Parameters
+    ----------
+    ac : float
+        Absolute carbon abundance A(C).
+    feh : float
+        Logarithmic iron-to-hydrogen abundance ratio [Fe/H].
+
+    Returns
+    -------
+    float
+        The carbon-to-iron abundance ratio [C/Fe].
+    """
     return ac - 8.43 - feh
