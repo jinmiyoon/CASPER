6d85ffd705624114d8ecdea2330bcf52d8ceae70
Taylor Webb <144720204+taylorw0525@users.noreply.github.com>
2025-07-21 13:02:11 -0400

    Apply suggestions from code review
    Co-authored-by: Jinmi Yoon <jinmiyoon@users.noreply.github.com>

--- tests/test_EW.py
+++ tests/test_EW.py
@@ -6,7 +6,7 @@ from casper.interface.EW import GBAND_QUAD
 
 def test_gband_quad():
     wave = np.linspace(4260, 4320, 1000)  # range is from 4260A to 4320A using 1000 points
-    flux = np.ones_like(wave)  # simulates a normalized flat continuum with no abosrption lines
+    flux = np.ones_like(wave)  # simulates a normalized flat continuum with no absorption lines
     flux[(wave > 4290) & (wave < 4300)] -= 0.1
     ew, ew_subtract = GBAND_QUAD(wave, flux)
     assert ew > 0
