b9fba87ce7ba630e2285d38aa3fd87f418fb9357
taylorw0525 <taylorw0525@gmail.com>
2025-07-21 11:37:26 -0400

    added type hints to spectrum

--- casper/interface/EW.py
+++ casper/interface/EW.py
@@ -6,6 +6,7 @@ from numpy.typing import ArrayLike
 from scipy.interpolate import interp1d
 
 from casper.interface import config
+from casper.interface.spectrum import Spectrum
 from casper.utils.logger_config import setup_logger
 
 logger = setup_logger(__name__)
@@ -129,7 +130,7 @@ def CAII_K18(wave: ArrayLike, flux: ArrayLike) -> float:
     return integrate.quad(func, 3924.7, 3942.7, limit=200, points=wave[(wave > 3924.7) & (wave < 3942.7)])[0]
 
 
-def get_KP_band(spectrum) -> Tuple[float, float]:
+def get_KP_band(spectrum: Spectrum) -> Tuple[float, float]:
     """
     Return the Ca II K line wavelength range for chi-square fitting based on Beers (1999),
     using measurements K6, K12, and K18.
@@ -171,7 +172,7 @@ def get_KP_band(spectrum) -> Tuple[float, float]:
         return np.nan
 
 
-def set_CH_procedure(spectrum) -> None:
+def set_CH_procedure(spectrum: Spectrum) -> None:
     """
     Set the carbon analysis mode for the given spectrum based on G-band strength.
 
